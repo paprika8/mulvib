d0e66e7605b67c24fcc69ccbf745c029d06c7eb1
paprika8 <aserstritt@gmail.com>
2026-05-07 21:08:05 +0800

    readd IReAdd

--- source/mpi/distribution.c
+++ source/mpi/distribution.c
@@ -3,130 +3,35 @@
 
 #define ll long long
 
-void nstDo(ll *Ic, ll NDo, ll *NMax)
-{
-	ll n = 0;
-d10:
-	Ic[n] = Ic[n] + 1;
-	if (Ic[n] <= NMax[n])
-		return;
-	else
-	{
-		Ic[n] = 1;
-		n = n + 1;
-		if (n <= NDo)
-			goto d10;
-	}
-}
-
-void MCoCal(ll num, ll *Ic, ll NDo, ll *NMax)
-{
-	ll out = num - 1;
-	ll i = 0;
-	for (; i < NDo; ++i)
-	{
-		Ic[i] = (out % NMax[i]) + 1;
-		out = out / NMax[i];
-		if (out == 0)
-		{
-			++i;
-			break;
-		}
-	}
-	for (; i < NDo; ++i)
-		Ic[i] = 1;
-}
-
-void getreaddr_(int *irank, ll *limQN, ll *Indxx, int *len, ll *n, ll *q_offset, ll *block_size)
+void getreaddr_(int *irank, int *n, ll *limQN, ll *lenLim, ll* readdr, ll* NMatx, ll *q_offset, ll *block_size, ll *conf_start, ll *conf_end)
 {
 	ll i;
-	ll *ic = malloc(sizeof(ll) * *len);
 	int limit_block = 1;
-	int limit = 1;
-	for (i = 0; i < *len; i++)
-	{
-		ic[i] = 1;
-		limit *= limQN[i];
-	}
 
-	char *buf = malloc(limit);
-	for (i = 0; i < limit; i++)
-		buf[i] = 0;
-
-	limit_block = limit / *n;
+	limit_block = *NMatx / *n;
 	i = limit_block * *irank;
 
-	if (*irank < limit % *n)
+	if (*irank < *NMatx % *n)
 		i += *irank;
 	else
-		i += limit % *n;
+		i += *NMatx % *n;
 
-	if (limit % *n > *irank)
+	if (*NMatx % *n > *irank)
 		limit_block++;
 
 	ll offset = i;
-	*q_offset = offset + 1;
+	*q_offset = offset + 1; //to fortran index
 	*block_size = limit_block;
+
+	int limit = 1;
+	for (i = 0; i < *lenLim; i++)
+		limit *= limQN[i];
+	i = 0;
+	offset ++; //to fortran index
+	for(; i < limit; ++i)
+		if(readdr[i] == offset) *conf_start = i + 1;
+	offset += limit_block - 1;
+	for(; i < limit; ++i)
+		if(readdr[i] == offset) *conf_end = i + 1;
 	return;
-
-	/*ll j;
-	MCoCal(i + 1, ic, *len, limQN);
-	for (i = 0; i < limit_block; i++)
-	{
-		for (int k = 0; k < *len; ++k)
-		{
-			if (ic[k] > 1)
-			{
-				j = i - Indxx[k] + offset;
-				buf[j] = 1;
-			}
-			if (ic[k] < limQN[k])
-			{
-				j = i + Indxx[k] + offset;
-				buf[j] = 1;
-			}
-		}
-		buf[i + offset] = 1;
-		nstDo(ic, *len, limQN);
-	}
-
-	ll res = buf[0];
-	ll n_offset = buf[0];
-	for (i = 1; i < limit; i++)
-	{
-		res += buf[i];
-		if (buf[i] && (i < 1 || !buf[i - 1]))
-		{
-			n_offset++;
-		}
-	}
-
-	ll *offset_p = malloc(n_offset * sizeof(ll) * 2 + sizeof(ll) * 2);
-	ll it = 0;
-	for (i = 0; i < limit; i++)
-	{
-		if (buf[i] && (i < 1 || !buf[i - 1]))
-			offset_p[it * 2] = i;
-		if (buf[i] && (i + 1 >= limit || !buf[i + 1]))
-		{
-			if (it)
-				offset_p[it * 2 + 1] = i - offset_p[it * 2] + 1 + offset_p[(it - 1) * 2 + 1];
-			else
-				offset_p[it * 2 + 1] = 0;
-			it++;
-			offset_p[it * 2] = i + 1;
-			offset_p[it * 2 + 1] =  -1;
-		}
-	}
-
-	printf("Block %lld res = %lld \\ %lld\n percent = %lf\n", *irank, res, limit, ((double)res) / limit);
-	printf("offset %lld\n", n_offset);
-	free(ic);
-	free(buf);
-	for(int i = 0; i < n_offset; ++i)
-		printf("%lld > readdr=%lld\n", offset_p[i * 2], offset_p[i * 2 + 1]);
-	free(offset_p); //TODO реадресация, удалить или внедрить
-*/
-
 }
-// b distribution.c:70
